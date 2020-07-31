d99312ff819c648823bf26c3486ce6ce2ce353f0
umitylmz <umit.yilmaz@agu.edu.tr>
2020-07-31 19:34:17 +0300

    wiki_data_is_added

--- fixy_app/app.py
+++ fixy_app/app.py
@@ -1,5 +1,5 @@
 
-from flask_wtf import FlaskForm
+#from flask_wtf import FlaskForm
 #from wtforms import StringField
 #from wtforms.validators import DataRequired
 #from flask_bootstrap import Bootstrap
@@ -8,13 +8,19 @@ from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 import pickle
+from keras.models import Model, Sequential
 import re
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from wtforms.validators import DataRequired
+import tensorflow as tf
+from keras.layers import Dense, LSTM, Flatten, Embedding, Dropout , Activation, GRU, Flatten, Input, Bidirectional, GlobalMaxPool1D, Convolution1D, TimeDistributed, Bidirectional
+from keras.layers.embeddings import Embedding
+#from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.feature_extraction.text import TfidfTransformer
+#from wtforms.validators import DataRequired
 import pandas as pd
 from os.path import join
-from jpype import JClass, JString, getDefaultJVMPath, shutdownJVM, startJVM,java
+#from jpype import JClass, JString, getDefaultJVMPath, shutdownJVM, startJVM,java
+
+'''
 ZEMBEREK_PATH: str = join('..', '..', 'bin', '/home/busra/İndirilenler/zemberek-full.jar')
 startJVM(
         getDefaultJVMPath(),
@@ -27,106 +33,76 @@ with open('templates/stopwords-tr.txt', 'r') as f:
 
 tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words=myList)
 
+'''
 
-
-app = Flask(__name__)
+#app = Flask(__name__)
 #Bootstrap(app)
-app.config['SECRET_KEY'] = "DontTellAnyone"
+#app.config['SECRET_KEY'] = "DontTellAnyone"
 
 '''
 class SearchForm(FlaskForm):
     name = StringField('query', validators=[DataRequired()])
 '''
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
-
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    output = [x for x in request.form.values()]
-    output=output[0]
-    output = sentiment(output)
-    return render_template('index.html',
-                           pred=output)
-
-
-@app.route('/emotion', methods=['GET', 'POST'])
-def emotional():
-    output = [x for x in request.form.values()]
-    output=output[0]
-    emo = emotion(output)
-    return render_template('index.html',
-                           emotion=emo)
-
-
-@app.route('/rcorrector', methods=['GET', 'POST'])
-def rcorrect():
-    output = [x for x in request.form.values()]
-    out = output[0]
-    print(out)
-    out = rule_based_corrector(out)
-    return render_template('index.html',
-                           rcorrect=out)
-
-
-@app.route('/formal', methods=['GET', 'POST'])
-def forrmal():
-    output = [x for x in request.form.values()]
-    output=output[0]
-    output = formal(output)
-    return render_template('index.html',
-                           formal=output)
 
 
 
-@app.route('/toformal', methods=['GET', 'POST'])
-def formall():
-    output = [x for x in request.form.values()]
-    output=output[0]
-    output = toFormal(output)
-    return render_template('index.html',
-                           formalform=output)
 
 
-
-@app.route('/MI_Mİ', methods=['GET', 'POST'])
-def mı_mi():
-    output = [x for x in request.form.values()]
-    output=output[0] #tüm inputu string halinde alıyor
-    output = check_1(output)
-    return render_template('index.html',
-                           ekli=output)
-
-@app.route('/DE_DA', methods=['GET', 'POST'])
-def de_da():
-    output = [x for x in request.form.values()]
-    output=output[0] #tüm inputu string halinde alıyor
-    output = check_2(output)
-    return render_template('index.html',
-                           ekli=output)
-
-@app.route('/Kİ', methods=['GET', 'POST'])
-def ki():
-    output = [x for x in request.form.values()]
-    output=output[0] #tüm inputu string halinde alıyor
-    output = check_3(output)
-    return render_template('index.html',
-                           ekli=output)
+embed_size = 128
+model = Sequential()
+model.add(Embedding(15000, embed_size))
+model.add(Bidirectional(LSTM(64, return_sequences = True)))
+model.add(Bidirectional(LSTM(32, return_sequences = True)))
+model.add(GlobalMaxPool1D())
+model.add(Dense(1, activation="sigmoid"))
 
 def check_1(text):#mı mi için model ile alına inputu düzeltcek
-    #işlemler
-    return text
+    with open('/home/ylmz/YlmzWorkSpace/fixy/ki_corrector/pre_trained_weights/tokenizer_ki.pickle', 'rb') as handle:
+        turkish_tokenizer = pickle.load(handle)
+    model.load_weights('/home/ylmz/YlmzWorkSpace/fixy/ki_corrector/pre_trained_weights/Model_ki.h5')
+    tokens = turkish_tokenizer.texts_to_sequences(text)
+    tokens_pad = pad_sequences(tokens, maxlen=7)
+    result = model.predict(tokens_pad)
+    print("dd")
+    return result
 
 def check_2(text):#de da için model ile alına inputu düzeltcek
-    #işlemler
-    return text
+    with open('fixy/pre_trained_weights/tokenizer_deda.pickle', 'rb') as handle:
+        turkish_tokenizer = pickle.load(handle)
+    model.load_weights('fixy/pre_trained_weights/Model_deda.h5')
+    
+    arr = text.split()
+
+    for i in range(len(arr)):
+        tmp = arr[i]
+        last2 = arr[i][-2:]
+        if(last2 == "de" or last2 == "da"):
+            print(i)
+            arr[i] = arr[i][:-2] + " x "
+            changed = ' '.join(arr[0:])
+            print(changed)
+            tokens = turkish_tokenizer.texts_to_sequences(changed)
+            tokens_pad = pad_sequences(tokens, maxlen=7)
+            print(tokens)
+            result = model.predict(tokens_pad)
+            if(result[0][0]>0.5):
+                
+    
+    
+    
+    
+
+    
+    print("dd")
+    return str(result[0])
 
 def check_3(text):#ki için model ile alına inputu düzeltcek
     #işlemler
     return text
 
-
+print(check_2("ben de seninle geleceğim"))
+'''
 def sentiment(text):
     negative = 0
     positive = 0
@@ -267,9 +243,4 @@ def toFormal(text):
     for analysis in analyses:
         formal = formal + str(converter.convert(analysis.surfaceForm(), analysis)).split("-")[0] + " "
     print(formal)
-    return formal
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return formal,'''
